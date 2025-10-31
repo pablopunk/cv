8b2f96d5601715348b23aa2f78e1f885df7f792e
Pablo P Varela <pablo@pablopunk.com>
2025-10-31 10:19:18 +0100

    vercel hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel'
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel(),
   integrations: [react(), tailwind()],
   server: {
     host: true,
